c1a1bd6ec88d28ff61c1aed5610e26705e918653
Zayar Tun <zay.t@hotmail.com>
2026-07-31 10:12:43 +0630

    revert: restore oil.nvim to pre-overhaul state

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -1,28 +1,20 @@
 return {
   'stevearc/oil.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  init = function()
-    vim.g.loaded_netrw = true
-    vim.g.loaded_netrwPlugin = true
 
-    vim.api.nvim_create_autocmd('VimEnter', {
-      once = true,
-      callback = function()
-        local arg = vim.fn.argv(0)
-        if arg ~= '' and vim.fn.isdirectory(arg) == 1 then
-          require('oil').open(arg)
-        end
-      end,
-    })
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  config = function()
+    require('oil').setup {
+      keymaps = {
+        ['<C-s>'] = false,
+        ['<C-v>'] = { 'actions.select', opts = { vertical = true } },
+      },
+
+      watch_for_changes = true,
+
+      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' }),
+    }
   end,
-  opts = {
-    keymaps = {
-      ['<C-s>'] = false,
-      ['<C-v>'] = { 'actions.select', opts = { vertical = true } },
-    },
-    watch_for_changes = true,
-  },
-  keys = {
-    { '-', '<CMD>Oil<CR>', desc = 'Open parent directory' },
-  },
 }
