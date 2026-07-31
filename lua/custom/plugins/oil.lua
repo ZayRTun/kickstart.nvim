f90b3b4f94358a321fd4f5f95e4456b0a6cd43bc
Zayar Tun <zay.t@hotmail.com>
2026-07-31 10:07:40 +0630

    feat: auto-open oil.nvim when launching with a directory argument

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -4,6 +4,16 @@ return {
   init = function()
     vim.g.loaded_netrw = true
     vim.g.loaded_netrwPlugin = true
+
+    vim.api.nvim_create_autocmd('VimEnter', {
+      once = true,
+      callback = function()
+        local arg = vim.fn.argv(0)
+        if arg ~= '' and vim.fn.isdirectory(arg) == 1 then
+          require('oil').open(arg)
+        end
+      end,
+    })
   end,
   opts = {
     keymaps = {
